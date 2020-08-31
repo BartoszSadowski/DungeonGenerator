748d2f80d7a2378f712f5f8ca2b81c2759e47e8a
Sadaffi <bartek_sadowski@o2.pl>
2020-08-31 22:08:37 +0200

    Added division of full dungeon

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const canvas = document.getElementById('demoCanvas');
 
 const getRandomValue = (min, max) =>  min + Math.floor(Math.random() * (max - min + 1));
 
+class Dimensions {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
 class Config {
-    constructor(minWidth, minHeight) {
-        this.minWdith = minWidth;
-        this.minHeight = minHeight;
+    constructor(divisable, minDimension) {
+        this.divisable = divisable;
+        this.minDimension = minDimension;
     }
 }
 
@@ -37,13 +44,13 @@ class Room {
         if (this.childRooms.length > 0) {
             throw new Error('This room has already been divided');
         }
-        const isWide = this.width > this.config.minWdith;
-        const isHigh = this.height > this.config.minWdith;
+        const isWide = this.width > this.config.divisable.width;
+        const isHigh = this.height > this.config.divisable.height;
 
         let divideVerticly;
 
         if (!isWide && !isHigh) {
-            throw new Error('This room is not dividable');
+            return 'Room is not dividable';
         } else if (isWide && isHigh) {
             divideVerticly = Math.random() >= 0.5;
         } else {
@@ -51,7 +58,7 @@ class Room {
         }
 
         if (divideVerticly) {
-            const newLine = getRandomValue(this.point1.x, this.point2.x);
+            const newLine = getRandomValue(this.point1.x + this.config.minDimension.width, this.point2.x - this.config.minDimension.width);
 
             this.childRooms.push(
                 new Room(
@@ -67,7 +74,7 @@ class Room {
                     this
                 ));
         } else {
-            const newLine = getRandomValue(this.point1.y, this.point2.y);
+            const newLine = getRandomValue(this.point1.y + this.config.minDimension.height, this.point2.y - this.config.minDimension.height);
 
             this.childRooms.push(
                 new Room(
@@ -83,5 +90,37 @@ class Room {
                     this
                 ));
         }
+
+        this.childRooms.forEach(room => {
+            room.divide();
+        });
+
+        return 'Room divided';
     }
 }
+
+class Dungeon extends Room {
+    constructor(dungeonPoint, config) {
+        super(
+            new Point(0, 0),
+            dungeonPoint,
+            config,
+            null
+        );
+    }
+}
+
+(function init(options){
+    const { divisable, minDimension, dungeonPoint } = options;
+    const config = new Config(divisable, minDimension);
+
+    const dungeon = new Dungeon(dungeonPoint, config);
+
+    dungeon.divide();
+
+    console.log(dungeon);
+})({
+    divisable: new Dimensions(10, 10),
+    minDimension: new Dimensions(3, 3),
+    dungeonPoint: new Point(50, 50)
+});
