5fd3dc631bace28973079ee0c6f536823c972e8c
Sadaffi <bartek_sadowski@o2.pl>
2020-10-22 19:50:55 +0200

    Added tests for line

---
+++ src/test/app/line.spec.ts
@@ -0,0 +1,73 @@
+import Line from '../../app/line';
+import Point from '../../app/point';
+import { AXIS } from '../../utils/dictionary';
+
+describe('rescale() - function returning new Line with scaled points', () => {
+    it('should return bigger points if given scale higher than 1', () => {
+        const point1 = new Point(1, 2);
+        const point2 = new Point(3, 4);
+        const scale = 2;
+        const line = new Line(point1, point2);
+
+        const scaledLine = line.rescale(scale);
+
+        expect(scaledLine.point1.x).toBe(point1.x * scale);
+        expect(scaledLine.point1.y).toBe(point1.y * scale);
+        expect(scaledLine.point2.x).toBe(point2.x * scale);
+        expect(scaledLine.point2.y).toBe(point2.y * scale);
+    });
+
+    it('should return smaller points if given scale is between 0 and 1', () => {
+        const point1 = new Point(2, 4);
+        const point2 = new Point(6, 8);
+        const scale = 0.5;
+        const line = new Line(point1, point2);
+
+        const scaledLine = line.rescale(scale);
+
+        expect(scaledLine.point1.x).toBe(point1.x * scale);
+        expect(scaledLine.point1.y).toBe(point1.y * scale);
+        expect(scaledLine.point2.x).toBe(point2.x * scale);
+        expect(scaledLine.point2.y).toBe(point2.y * scale);
+    });
+
+    it('should return same points if given scale equal to 1', () => {
+        const point1 = new Point(1, 2);
+        const point2 = new Point(3, 4);
+        const scale = 1;
+        const line = new Line(point1, point2);
+
+        const scaledLine = line.rescale(scale);
+
+        expect(scaledLine.point1.x).toBe(point1.x);
+        expect(scaledLine.point1.y).toBe(point1.y);
+        expect(scaledLine.point2.x).toBe(point2.x);
+        expect(scaledLine.point2.y).toBe(point2.y);
+    });
+});
+
+describe('get axis() - retrieves axis in which line is layed', () => {
+    it('should return VERTICAL if line is layed verticaly', () => {
+        const point1 = new Point(2, 5);
+        const point2 = new Point(2, 7);
+        const line = new Line(point1, point2);
+
+        expect(line.axis).toBe(AXIS.VERTICAL);
+    });
+
+    it('should return HORIZONTAL if line is layed horizontaly', () => {
+        const point1 = new Point(5, 2);
+        const point2 = new Point(7, 2);
+        const line = new Line(point1, point2);
+
+        expect(line.axis).toBe(AXIS.HORIZONTAL);
+    });
+
+    it('should return UNDEFINED if line is neither', () => {
+        const point1 = new Point(5, 4);
+        const point2 = new Point(7, 2);
+        const line = new Line(point1, point2);
+
+        expect(line.axis).toBe(AXIS.UNDEFINED);
+    });
+});
