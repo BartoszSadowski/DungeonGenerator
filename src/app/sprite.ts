2d89da528b8d30380e7911c91df278bc2d7a073b
Sadaffi <bartek_sadowski@o2.pl>
2020-10-27 17:14:42 +0100

    Added non-static methods to Sprite

--- src/app/sprite.ts
+++ src/app/sprite.ts
@@ -1,10 +1,31 @@
 import Point from '../utils/point';
+import Dimensions from '../utils/dimensions';
 
 import { anyFunction } from '../types/functions';
 
 export default class Sprite {
     static image: HTMLImageElement = new Image();
-    point: Point;
+    anchor: Point;
+    size: Dimensions;
+
+    constructor(anchor: Point, size: Dimensions) {
+        this.anchor = anchor;
+        this.size = size;
+    }
+
+    draw(ctx: CanvasRenderingContext2D, spot: Point, dimensions: Dimensions) {
+        ctx.drawImage(
+            Sprite.image,
+            this.anchor.x,
+            this.anchor.y,
+            this.size.width,
+            this.size.height,
+            spot.x,
+            spot.y,
+            dimensions.width,
+            dimensions.height
+        );
+    }
 
     static initialize(tileMap: string, callBack: anyFunction) {
         this.image.addEventListener('load', callBack);
