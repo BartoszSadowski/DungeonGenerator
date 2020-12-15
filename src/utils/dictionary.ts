5b9459b5512f2d6e72e60e85c3ea45b27755d71d
Sadaffi <bartek_sadowski@o2.pl>
2020-12-15 21:38:55 +0100

    Added new sprite map

--- src/utils/dictionary.ts
+++ src/utils/dictionary.ts
@@ -19,7 +19,12 @@ export const SPRITE_TYPES: Record<string, string> = {
     WALL: 'WALL',
     DOOR: 'DOOR',
     ENTERANCE: 'ENTERANCE',
-    EXIT: 'EXIT'
+    EXIT: 'EXIT',
+    EVENT_A: 'EVENT_A',
+    EVENT_B: 'EVENT_B',
+    EVENT_C: 'EVENT_C',
+    EVENT_D: 'EVENT_D',
+    EVENT_E: 'EVENT_E'
 };
 
 export enum Items {
--- src/utils/spriteLocations.ts
+++ src/utils/spriteLocations.ts
@@ -4,9 +4,14 @@ import { SPRITE_TYPES } from './dictionary';
 const SPRITE_LOCATIONS: Record<string, Point> = {
     [SPRITE_TYPES.BASE]: new Point(0, 0),
     [SPRITE_TYPES.WALL]: new Point(16, 0),
-    [SPRITE_TYPES.DOOR]: new Point(0, 16),
-    [SPRITE_TYPES.ENTERANCE]: new Point(16, 16),
-    [SPRITE_TYPES.EXIT]: new Point(0, 32)
+    [SPRITE_TYPES.DOOR]: new Point(32, 0),
+    [SPRITE_TYPES.ENTERANCE]: new Point(0, 16),
+    [SPRITE_TYPES.EXIT]: new Point(16, 16),
+    [SPRITE_TYPES.EVENT_A]: new Point(32, 16),
+    [SPRITE_TYPES.EVENT_B]: new Point(0, 32),
+    [SPRITE_TYPES.EVENT_C]: new Point(16, 32),
+    [SPRITE_TYPES.EVENT_D]: new Point(32, 32),
+    [SPRITE_TYPES.EVENT_E]: new Point(0, 48)
 };
 
 export default SPRITE_LOCATIONS;
